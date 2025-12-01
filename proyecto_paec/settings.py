9346c1547ff0057674d3039135f2d29e552c1e3e
Man <german24491@cbtis75.edu.mx>
2025-12-01 17:44:25 -0600

    m

--- proyecto_paec/settings.py
+++ proyecto_paec/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 # 3. RUTA DE RECOLECCIÓN: Usa os.path.join para máxima compatibilidad
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") # <--- SOLUCIÓN AL ERROR
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root") # <--- SOLUCIÓN AL ERROR
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
